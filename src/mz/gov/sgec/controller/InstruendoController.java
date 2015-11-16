b2f0e0ebc6afeb86c735512d928c0fb96fe9d7c0
Moaine <albertocremildomoiane@gmail.com>
2015-11-16 11:16:50 +0200

    2feira actualizacao instruendo - andando

--- src/mz/gov/sgec/controller/InstruendoController.java
+++ src/mz/gov/sgec/controller/InstruendoController.java
@@ -3,14 +3,6 @@ package mz.gov.sgec.controller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-/*
-import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.select.SelectorComposer;
-import org.zkoss.zk.ui.select.annotation.Listen;
-import org.zkoss.zk.ui.select.annotation.Wire;
-import org.zkoss.zul.Window;
-*/
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.util.Clients;
@@ -27,10 +19,12 @@ import org.zkoss.zul.Datebox;
 import org.zkoss.zul.Intbox;
 import org.zkoss.zul.Listbox;
 import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listheader;
 import org.zkoss.zul.Listitem;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Radio;
 import org.zkoss.zul.Radiogroup;
+import org.zkoss.zul.Spinner;
 import org.zkoss.zul.Textbox;
 
 import mz.gov.sgec.dao.InstruendoDAO;
@@ -41,32 +35,34 @@ import mz.gov.sgec.model.Turma;
 public class InstruendoController extends GenericForwardComposer {
 		
 		//private Div registo;
-		//private Div listar;
-		private Textbox txt_rapelido;		
-		private Textbox txt_rapelido_pai;
-		private Textbox txt_rapelido_mae;		
-		private Textbox txt_rcodigo_barra;
-		private Textbox txt_rcelular;
-		private Textbox cb_rcivil;
-		private Radiogroup txt_rgenero;
-		private Intbox itx_rinteiro;
-		private Intbox itx_rdecimal;
-		private Datebox d_rnascimento;
-		private Textbox txt_rnaturalidade;
-		private Textbox txt_rnome;
-		private Textbox txt_rnome_pai;
-		private Textbox txt_rnome_mae;
-		private Textbox txt_rnumero_bi;
-		private Textbox txt_rresidencia;
-		private Combobox cb_rtipo_carta;	
-		private Datebox d_rvalidade_bi;
+		private Textbox txt_apelido;		
+		private Textbox txt_apelido_pai;
+		private Textbox txt_apelido_mae;		
+		private Textbox txt_codigo_barra;
+		private Textbox txt_celular;
+		private Textbox cb_civil;
+		private Radiogroup rd_genero;
+		private Spinner sp_inteiro;
+		private Spinner sp_decimal;
+		private Datebox d_nascimento;
+		private Textbox txt_naturalidade;
+		private Textbox txt_nome;
+		private Textbox txt_nome_pai;
+		private Textbox txt_nome_mae;
+		private Textbox txt_numero_bi;
+		private Textbox txt_residencia;
+		private Combobox cb_tipo_carta;	
+		private Datebox d_validade_bi;
 		private Listbox lst_instruendo;
-		
-		private Button btn_rreg;
-		private Button btn_rturma;
-		private Button btn_rpagamento;
+
+		private Button btn_reg;
+		private Button btn_turma;
+		private Button btn_pagamento;
+		private Button btn_actualizar;
+		//private Button btn_act;
 		private TurmaDAO tdao = new TurmaDAO();
 		private InstruendoDAO idao = new InstruendoDAO();
+		Instruendo instruendo = new Instruendo();
 		
 		public void onClick$btn_rpagamento(Event e){
 			Executions.sendRedirect("instruendo_pagamento.zul");
@@ -75,52 +71,76 @@ public class InstruendoController extends GenericForwardComposer {
 			Executions.sendRedirect("instruendo_alocar.zul");
 		}
 		
-		public void onClick$btn_rreg(Event e){
-			Instruendo instruendo = new Instruendo();
-			
+		public void onClick$btn_reg(Event e){
 			instruendo.setId((long)(Math.random()*1000000)+1);
-			//instruendo.setAltura(itx_rinteiro.getValue()+""+itx_rdecimal.getValue());
-			instruendo.setApelido(txt_rapelido.getText());
-			instruendo.setApelido_pai(txt_rapelido_pai.getText());
-			instruendo.setApelido_mae(txt_rapelido_mae.getText());
-			instruendo.setBi(txt_rnumero_bi.getText());
-			instruendo.setCodigo_barra(txt_rcodigo_barra.getText());
-			instruendo.setData_nascimento(d_rnascimento.getValue());			
-			instruendo.setEstado_civil(cb_rcivil.getText());
-			//instruendo.setGenero(((Radio) txt_rgenero) getValue());
-			instruendo.setNaturalidade(txt_rnaturalidade.getText());
-			instruendo.setNome(txt_rnome.getText());
-			instruendo.setNome_mae(txt_rnome_mae.getText());
-			instruendo.setNome_pai(txt_rnome_pai.getText());
-			instruendo.setResidencia(txt_rresidencia.getText());
-			instruendo.setTelefone(txt_rcelular.getText());
-			instruendo.setTipo_carta(cb_rtipo_carta.getValue());
-			instruendo.setValidade_bi(d_rvalidade_bi.getValue());
+			instruendo.setAltura(Double.parseDouble(sp_inteiro.getValue()+"."+sp_decimal.getValue()));
+			instruendo.setApelido(txt_apelido.getText());
+			instruendo.setApelido_pai(txt_apelido_pai.getText());
+			instruendo.setApelido_mae(txt_apelido_mae.getText());
+			instruendo.setBi(txt_numero_bi.getText());
+			instruendo.setCodigo_barra(txt_codigo_barra.getText());
+			instruendo.setData_nascimento(d_nascimento.getValue());			
+			instruendo.setEstado_civil(cb_civil.getText());
+			instruendo.setGenero(rd_genero.getSelectedItem().getValue().toString());
+			instruendo.setNaturalidade(txt_naturalidade.getText());
+			instruendo.setNome(txt_nome.getText());
+			instruendo.setNome_mae(txt_nome_mae.getText());
+			instruendo.setNome_pai(txt_nome_pai.getText());
+			instruendo.setResidencia(txt_residencia.getText());
+			instruendo.setTelefone(txt_celular.getText());
+			instruendo.setTipo_carta(cb_tipo_carta.getValue());
+			instruendo.setValidade_bi(d_validade_bi.getValue());
 						
 			idao.create(instruendo);			
 			Clients.showNotification("Registo de Instruendo"); 
 			clear();
 		}
+
+		public void onClick$btn_actualizar(){
+			long id = 2;
+			instruendo.setId(id);
+			instruendo.setAltura(Double.parseDouble(sp_inteiro.getValue()+"."+sp_decimal.getValue()));
+			instruendo.setApelido(txt_apelido.getText());
+			instruendo.setApelido_pai(txt_apelido_pai.getText());
+			instruendo.setApelido_mae(txt_apelido_mae.getText());
+			instruendo.setBi(txt_numero_bi.getText());
+			instruendo.setCodigo_barra(txt_codigo_barra.getText());
+			instruendo.setData_nascimento(d_nascimento.getValue());			
+			instruendo.setEstado_civil(cb_civil.getText());
+			instruendo.setGenero(rd_genero.getSelectedItem().getValue().toString());
+			instruendo.setNaturalidade(txt_naturalidade.getText());
+			instruendo.setNome(txt_nome.getText());
+			instruendo.setNome_mae(txt_nome_mae.getText());
+			instruendo.setNome_pai(txt_nome_pai.getText());
+			instruendo.setResidencia(txt_residencia.getText());
+			instruendo.setTelefone(txt_celular.getText());
+			instruendo.setTipo_carta(cb_tipo_carta.getValue());
+			instruendo.setValidade_bi(d_validade_bi.getValue());
+			
+			idao.update(instruendo);			
+			Clients.showNotification("Actualiacao efectuada com Instruendo"); 
+			clear();
+		}
 		
 		public void clear() {
-			//instruendo.setAl
-			txt_rapelido.setText(" ");
-			txt_rapelido_pai.setText(" ");
-			txt_rapelido_mae.setText(" ");
-			txt_rnumero_bi.setText(" ");
-			txt_rcodigo_barra.setText(" ");
-			txt_rnome.setText(" ");
-			txt_rnome_mae.setText(" ");
-			txt_rnome_pai.setText(" ");
-			txt_rresidencia.setText(" ");
-			txt_rcelular.setText(" ");
-			txt_rnaturalidade.setText(" ");
-			cb_rcivil.setText(" ");
-			cb_rtipo_carta.setText(" ");
-			d_rvalidade_bi.setText(null);			
-			d_rnascimento.setText(null);
-
-			//instruendo.setG
+			sp_inteiro.setValue(0);
+			sp_decimal.setValue(0);
+			txt_apelido.setText(" ");
+			txt_apelido_pai.setText(" ");
+			txt_apelido_mae.setText(" ");
+			txt_numero_bi.setText(" ");
+			txt_codigo_barra.setText(" ");
+			txt_nome.setText(" ");
+			txt_nome_mae.setText(" ");
+			txt_nome_pai.setText(" ");
+			txt_residencia.setText(" ");
+			txt_celular.setText(" ");
+			txt_naturalidade.setText(" ");
+			rd_genero.setSelectedItem(null);
+			cb_civil.setText(" ");
+			cb_tipo_carta.setText(" ");
+			d_validade_bi.setText(null);			
+			d_nascimento.setText(null);
 		}
 		
 		public List <Turma> getTurmas(){
@@ -133,25 +153,9 @@ public class InstruendoController extends GenericForwardComposer {
 		
 		public Instruendo getInstruendo(){	
 			Instruendo inst = new Instruendo();
-			long id = 1;
+			long id = 2;
 			inst.setId(id);
 			return idao.findById(inst.getId());
 		}
 		
-	/*	private static final long serialVersionUID = 1L;
-	     
-		    @Wire
-		    Window modalDialog;
-		     
-		    @Listen("onClick = #closeBtn")
-		public void showModall(Event e) {
-		        modalDialog.detach();
-		}*/
-		
-	/*	@Listen("onClick = #orderBtn")
-		public void showModal(Event e) {
-		        //create a window programmatically and use it as a modal dialog.
-		        Window window = (Window)Executions.createComponents("instruendo_pagamento.zul", null, null);
-		        window.doModal();
-		}*/
 }
