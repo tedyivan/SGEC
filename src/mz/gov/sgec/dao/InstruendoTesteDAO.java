be99651a3d236e95642ccbd65a7d4a7c6f8f95b4
Elton Laice <eltonlaice@sapo.mz>
2015-11-21 02:15:31 +0200

    Criacao de classes DAO

---
+++ src/mz/gov/sgec/dao/InstruendoTesteDAO.java
@@ -0,0 +1,12 @@
+package mz.gov.sgec.dao;
+
+import mz.gov.sgec.model.InstruendoTeste;
+
+public class InstruendoTesteDAO extends GenericDAO<InstruendoTeste>{
+
+	public InstruendoTesteDAO() {
+		// TODO Auto-generated constructor stub
+		super(InstruendoTeste.class);
+	}
+
+}
